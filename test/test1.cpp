deabed8f9b7964bcff1eaa1a5de2336ae1369316
Alexey V. Medvedev <alexey.v.medvedev@gmail.com>
2018-04-05 16:02:32 +0300

    Project description test corrected in source files headers

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,7 +1,7 @@
 /*
     Copyright (C) 2011-2013,2018 Alexey V. Medvedev
 
-    This file is part of cppcgen. cppcgen is a C++-11 generator of C code with macroses.
+    This file is part of cppcgen. cppcgen is a generator of C code written in C++-11.
 
     cppcgen is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
